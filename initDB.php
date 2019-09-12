692c9997207aab0c269b98f41dd8858ec23df77b
jj484 <jj484@njit.edu>
2019-09-12 12:33:42 -0400

    updated initDB.php

--- initDB.php
+++ initDB.php
@@ -1,4 +1,15 @@
 <?php
 require('config.php');
 echo $host;
+
+$connection_string = "mysql:host=$host;dbname=$database;username=$username;
+password=$password;charset=utf8mb4";
+
+try{
+	$db = new PDO($connection_string);
+	echo "Should have connected";
+} catch {
+	echo $e->getMessage();
+	exit("It didn't work");
+}x
 ?>
