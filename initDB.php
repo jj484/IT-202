03ad40f53805e5bf2d10587ea8a65739a06898bd
jj484 <54951355+jj484@users.noreply.github.com>
2019-09-17 11:59:32 -0400

    Update initDB.php

--- initDB.php
+++ initDB.php
@@ -1,19 +1,27 @@
 <?php
-ini_set('display errors', 1);
-ini_set('display_startup_errors, 1');
-error_report(E_ALL);
-
+#turn error reporting on
+ini_set('display_errors',1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+//pull in config.php so we can access the variables from it
 require('config.php');
-echo $host;
-
-$connection_string = "mysql:host=$host;dbname=$database;username=$username;
-password=$password;charset=utf8mb4";
-
+echo "Loaded Host: " . $host;
+$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
 try{
-	$db = new PDO($connection_string);
-	echo "Should have connected";
-} catch {
+	$db = new PDO($conn_string, $username, $password);
+	echo "Connected";
+	$query = "create table if not exists `TestUsers`(
+		`id` int auto_increment not null,
+		`username` varchar(30) not null unique,
+		`pin` int default 0,
+		PRIMARY KEY (`id`)
+		) CHARACTER SET utf8 COLLATE utf8_general_ci";
+	$stmt = $db->prepare($query);
+	$r = $stmt->execute();
+	echo "<br>" . $r . "<br>";
+}
+catch(Exception $e){
 	echo $e->getMessage();
-	exit("It didn't work");
-}x
+	exit("Something went wrong");
+}
 ?>
