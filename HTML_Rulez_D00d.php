0e7a1b3c717d7852f265d3231d1ce0d915cf4a3d
jj484 <jj484@njit.edu>
2019-10-03 12:43:10 -0400

    modified HTML_Rulez_D00d

---
+++ HTML_Rulez_D00d.php
@@ -0,0 +1,62 @@
+
+<!DOCTYPE html>
+
+
+<?php
+ini_set('display_errors',1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+function getName(){
+	if(isset($_GET['name'])){
+		echo "<p>Hello, " . $_GET['name'] . "</p>";
+	}
+}
+?>
+
+
+<html>
+  <head>
+
+
+
+
+  </head>
+
+
+  <body>
+    <script>
+    var myD = document.createElement("div");
+    var myP = document.createElement("p"); 
+    myP.innerText = 'new element added'; 
+    document.body.appendChild(myD);
+    myD.appendChild(myP);
+    </script>
+
+	<?php getName();?>
+	<form mode="GET" action="#">
+	<input name="name" type="text" placeholder="Enter your name"/>
+	<input name="number" type="number" placeholder="Enter a number"/>
+	<input name="password" type="password" placeholder="Enter a FAKE password"/>
+	
+	<label for="YES">Yes<label>
+	<input type="radio" name="radio" id="No" value="No"/>
+
+	<select name="dropdown">
+	<option value="1">One</option>
+	<option value="2">Two</option>
+	<option value="3">Three</option>
+	</select>
+	<input type="checkbox" name="check"/>
+	<textarea name="text"></textarea>
+	<input type="date" name="date"/> 
+	
+	?>
+
+  </body>
+
+
+</html>
+
+
+
+
--- index.php
+++ index.php
@@ -1,8 +1,11 @@
-w<html>
+<html>
 
   <a href="../IT-202"> IT202 </a> <br>
   <a href="../IS117"> IS117 </a> <br>
-  <a href="https://web.njit.edu/~at633"> Alan's a b****. </a> <br>
+  <a href="https://web.njit.edu/~at633"> Aaron </a> <br>
+  <a href="https://web.njit.edu/~zmt6"> Zack </a> <br>
+  <a href="https://web.njit.edu/~wc347"> Alan <a> <br>
+  <a href="HTML_Rulez_D00d.php"> HTML Rulez D00d </a> <br>
   <a href="handleRequestData.php?number1=ENTER%20A%20VALUE%20HERE&number2=ENTER%20A%20VALUE%20HERE"> handleRequestData </a> <br>
 
 
