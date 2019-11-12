d5edf9a80664adfe3875652f22e931e01c993d44
jj484 <jj484@njit.edu>
2019-11-12 12:12:05 -0500

    Oh you know

--- action.php
+++ action.php
@@ -1,27 +1,19 @@
-<html>
 
-  <!--
-    RECEIVED HELP FROM NEVIN B
-    AND FROM ONLINE TUTORIAL FOUND AT:
-    https://www.youtube.com/watch?v=arqv2YVp_3E
-  -->
-</html>
 
-/*
 
-ALTERNATIVE METHOD I GOT WORKING!
-ISN'T WHAT IS CURRENTLY LIVE ON THE SITE
-***HELPED ZACH BY USING MY CODE BELOW
 
 <?php
 require('config.php');
 $conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
 
+echo $conn_string."<br>";
 $db = new PDO($conn_string, $username, $password);
 
 $stm = $db->query("select * from users");
 $result = $stm->fetch();
+
 echo $result['username'];
+echo $result['password'];
 
 if ($result['username'] == $_POST['usernameLog'] && $result['password'] == $_POST['passwordLog'] )
 {
@@ -31,38 +23,5 @@ else {
   echo "Login Failed";
 }  
 
-*/
-  
 
-	$username = $_POST['usernameLog'];
-	$password = $_POST['passwordLog'];
-  
-  	$link = mysql_connect("sql1.njit.edu", "jj484","fXm4MFaae");
-	mysql_select_db("jj484");
-
-   if (!$link) {
-    die('Could not connect: ' . mysql_error());
-   }
-
-		$result = mysql_query("select * from users where username = '$username'") or die("failed to query database " .mysql_error());
-
-		$row = mysql_fetch_array($result); 
-  echo "Database username: ".$row['username']."<br>";
-  echo "Entered username: ".$_POST['usernameLog']."<br>";
-  
-  $result = mysql_query("select * from users where password = '$password'") or die("failed to query database " .mysql_error());
-  
-  echo "Database password: ".$row['password']."<br>";
-  echo "Entered password: ".$_POST['passwordLog']."<br>";
-		if ($row['username'] == $username && $row['password'] == $password )
-		{
-				echo "Login Succeded";
-		}
-
-		else {
-			echo "Login Failed";
-		}
-
-		
-		?>  
-
+?>
---
+++ project.backup2.php
@@ -0,0 +1,27 @@
+<html>
+
+  <head>
+    
+      <p> <h1> Login Page </h1> <p>
+      
+      <p> 
+        <form method="POST" action="action.php" >
+          Username:
+          <input type="text" name="usernameLog" id="usernameLog" placeholder="Username">  <br>
+          PIN:
+          <input type="password" name="passwordLog" id="passwordLog" placeholder="PIN"> <br>
+          
+          
+          
+          <!--
+          <input type="password" name="password" placeholder="Enter password"/>
+          <input type="password" name="confirm" placeholder="Re-Enter password"/>
+          -->
+          
+          <input type="submit" value="Login"/>
+          
+        </form>
+      
+      </p>
+
+</html>
